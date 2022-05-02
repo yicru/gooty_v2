c81b44cd10d8fda08a03f53d66ff934a6fcdc779
yuichiro miyamae <yu1ro.mya@gmail.com>
2022-05-02 18:20:35 +0900

    add: cafe 取得クエリ

--- next/src/pages/api/graphql.ts
+++ next/src/pages/api/graphql.ts
@@ -1,9 +1,46 @@
 import { createServer } from '@graphql-yoga/node'
 import { NextApiRequest, NextApiResponse } from 'next'
 
+import { prisma } from '../../lib/prisma'
+
 const server = createServer<{
   req: NextApiRequest
   res: NextApiResponse
-}>()
+}>({
+  schema: {
+    resolvers: {
+      Query: {
+        cafes: () =>
+          prisma.cafe.findMany({
+            include: {
+              images: true,
+            },
+          }),
+      },
+    },
+    typeDefs: /* GraphQL */ `
+      type Image {
+        url: String!
+      }
+
+      type Cafe {
+        id: String!
+        name: String!
+        price: Int!
+        openingHours: String!
+        address: String!
+        tel: String!
+        latitude: String!
+        longitude: String!
+        videoUrl: String!
+        images: [Image!]!
+      }
+
+      type Query {
+        cafes: [Cafe!]!
+      }
+    `,
+  },
+})
 
 export default server
